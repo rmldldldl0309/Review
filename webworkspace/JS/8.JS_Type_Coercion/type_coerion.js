6a2438246350c607e23f852d7d53cd0ac6ae4b21
rmldldldl0309 <kimsangheon0309@gmail.com>
2024-03-19 14:03:42 +0900

    2024-03-19 type_coercion

--- webworkspace/JS/8.JS_Type_Coercion/type_coerion.js
+++ webworkspace/JS/8.JS_Type_Coercion/type_coerion.js
@@ -22,8 +22,10 @@ console.log('==============================');
 
 /*
     불리언 타입으로 형변환
-    - 
+    - '', NaN, null, undefined는 false, 그 이외의 데이터에 대해서는 true로 변경하여 연산을 진행
 */
+if(null) console.log('null')
+if(!null) console.log('not null')
 
 
 
@@ -35,41 +37,54 @@ console.log('==============================');
 
 /*
     문자열 타입으로 형변환
-    - 
-    - 
+    - String 클래스 생성자를 new 연산자 없이 사용
+    - 데이터.toString()
+    - 데이터 + ''
 */
+console.log(String(10));
+console.log(typeof String(10));
 
+console.log((10).toString());
+console.log(typeof (10).toString());
 
+console.log(10 + '');
+console.log(typeof (10 + ''));
 
 console.log('==============================');
 
 /*
     숫자 타입으로 형변환
-    - 
-    - 
+    - Number 클래스의 생성자를 new연산자 없이 사용
+    - 문자열일 경우 parseInt(), parseFloat()메서드 사용 가능
+    - + 단항 연산자 사용 가능
 */
+console.log(Number(false))
+console.log(typeof Number(false))
 
-
+console.log(parseInt('10'))
+console.log(typeof parseInt('10'))
 
 console.log('==============================');
 
 /*
     불리언 타입으로 형변환
-    - 
-    - 
+    - Boolean 클래스의 생성자를 new연산자 없이 사용
+    - !! 사용 가능
 */
+console.log(Boolean('문자열'))
+console.log(typeof Boolean('문자열'))
 
-
+console.log(!!'문자열')
 
 console.log('==============================');
 
 /*
     단축 평가 
-    - 
-    - 
-    - 
-    - 
-    - 
+    - 논리 연산자는 논리 결과를 결정한 피연산자의 결과를 그대로 반환
+    - true && data = data
+    - false && data = false
+    - true || data = true
+    - false || data = data
 */
-
+console.log('홍길동' || false)
 
