83de118cbe86507973a8e6887dd9b23decffc1aa
rmldldldl0309 <kimsangheon0309@gmail.com>
2024-03-29 17:43:36 +0900

    화살표 함수

---
+++ webworkspace/ES6+/3.arrow_function/arrow_function.js
@@ -0,0 +1,71 @@
+/*
+    화살표 함수 : 
+    - function 키워드 대신 화살표( => )를 사용하여 함수를 생성하는 방법 (기본적으로 일회성, 익명함수)
+*/
+let func = function (arg1, arg2) {
+
+}
+
+func = (arg1, arg2) => {
+
+}
+
+/* 
+    매개변수 지정 방법 
+*/
+
+// 매개변수가 없을 경우
+func = () => {   };
+
+// 매개변수가 하나인 경우, 매개변수 위치의 소괄호를 제거할 수 있다
+func = (arg) => {   };
+func = arg => {   };
+
+// 매개변수가 여러개일 경우, 매개변수 위치의 소괄호를 제거할 수 없다
+func = (arg1, arg2) => {   };
+// func = arg1, arg2 => {   };         // 2개의 구문 생성
+
+/*
+    구현부 지정 방법
+*/
+
+//  single line block
+func = (arg) => {return arg * arg};
+
+// 구현부가 한줄이면 중괄호 생략 가능, 해당 구문의 결과를 return
+func = arg => arg * arg;
+
+// 구현부가 한줄이면서 객체 리터럴을 반환할 때 함수의 중괄호를 제거하면 객체 리터럴을 소괄호로 묶어야 한다
+func = (name, tombName) => {
+    return {
+        name: name,
+        tombName: tombName
+    };
+}
+
+func = (name, tombName) => {
+    return {
+        name,
+        tombName
+    };
+}
+
+func = (name, tombName) => ({
+    name,
+    tombName
+});
+
+/*
+    화살표 함수의 호출 : 
+    - 화살표 함수는 익명함수로만 사용할 수 있기 때문에 함수표현식 형태로 변수에 저장하여 사용
+    - 콜백함수에 많이 사용됨
+*/
+const pow = (x) => x*x;
+console.log(pow(10));
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.forEach(function (value, index) {
+    console.log(`index:${index}, value:${value}`);
+})
+
+numbers.forEach((value, index) => console.log(`index:${index}, value:${value}`));
