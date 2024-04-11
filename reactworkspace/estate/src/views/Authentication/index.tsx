f719d84ca6938055eca483f109f55fecff22e8ed
rmldldldl0309 <kimsangheon0309@gmail.com>
2024-04-11 09:53:06 +0900

    회원가입 버튼 클릭 & 로그인 버튼 클릭

--- reactworkspace/estate/src/views/Authentication/index.tsx
+++ reactworkspace/estate/src/views/Authentication/index.tsx
@@ -3,7 +3,12 @@ import './style.css'
 
 type AuthPage = 'sign-in' | 'sign-up';
 
-function SignIn () {
+interface Props {
+    // 함수의 타입 선언 시 > 화살표 함수로 매개변수 + 리턴 값 입력
+    onLinkClickHandler: () => void;
+}
+
+function SignIn ({onLinkClickHandler}: Props) {
     const onSignInButtonClickHandler = () => {
 
     };
@@ -13,7 +18,7 @@ function SignIn () {
             <div className='authentication-input-container'></div>
             <div className='authentication-button-container'>
                 <div className="primary-button full-width" onClick={onSignInButtonClickHandler}>로그인</div>
-                <div className="text-link">회원가입</div>
+                <div className="text-link" onClick={onLinkClickHandler}>회원가입</div>
             </div>
             <div className='short-divider'></div>
             <div className='authentication-sns-container'></div>
@@ -21,16 +26,34 @@ function SignIn () {
     )
 }
 
-function SignUp () {
+function SignUp ({onLinkClickHandler}: Props) {
 
-    return (<>회원가입</>)
+    const onSignInButtonClickHandler = () => {};
+
+    return (
+        <div className='authentication-contents'>
+            <div className='authentication-sns-container'></div>
+            <div className='short-divider'></div>
+            <div className='authentication-input-container'></div>
+            <div className='authentication-button-container'>
+                <div className="primary-button full-width" onClick={onSignInButtonClickHandler}>회원가입</div>
+                <div className="text-link" onClick={onLinkClickHandler}>로그인</div>
+            </div>
+        </div>
+    )
 }
 
 export default function Authentication () {
 
+    // use- 함수 = 훅함수 > 반드시 컴포넌트 바로 아래에 선언되어 있어야 한다
     const [page, setPage] = useState<AuthPage>('sign-in');
 
-    const AuthenticationContents = page === 'sign-in' ? <SignIn/> : <SignUp/>;
+    const onLinkClickHandler = () => {
+        if (page === 'sign-in') setPage('sign-up');
+        else setPage('sign-in');
+    }
+
+    const AuthenticationContents = page === 'sign-in' ? <SignIn onLinkClickHandler={onLinkClickHandler}/> : <SignUp onLinkClickHandler={onLinkClickHandler}/>;
 
     return (
         <div id='authentication-wrapper'>
