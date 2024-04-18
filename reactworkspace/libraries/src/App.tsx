978464fc268844f395b5a6d4911ac7ce44831ee5
rmldldldl0309 <kimsangheon0309@gmail.com>
2024-04-18 14:06:53 +0900

    2024-04-17

--- reactworkspace/libraries/src/App.tsx
+++ reactworkspace/libraries/src/App.tsx
@@ -3,6 +3,9 @@ import './App.css';
 import ZustandLibrary from 'src/zustand/ZustandLibrary';
 import { Route, Routes } from 'react-router';
 import ReactRouterLibrary, { ReactRouterPage1, ReactRouterPage2 } from './react-router/ReactRouterLibrary';
+import Layout from './react-router/Layout';
+import AxiosLibrary from './axios/AxiosLibrary';
+import ReactCookieLibrary from './react-cookie/ReactCookieLibrary';
 
 function App() {
   return (
@@ -12,12 +15,16 @@ function App() {
       <Route path='/page2' element={<ReactRouterPage2/>}/>
       <Route path='/zustand' element={<ZustandLibrary/>}/> */}
 
-      <Route path='/router'>
+      <Route path='/router' element={<Layout/>}>
         <Route index element={<ReactRouterLibrary />} />
-        <Route path='page1' element={<ReactRouterPage1/>}/>
+        <Route path='page1/:value' element={<ReactRouterPage1/>}/>
         <Route path='page2' element={<ReactRouterPage2/>}/>
       </Route>
       <Route path='/zustand' element={<ZustandLibrary/>}/>
+      <Route path='/axios' element={<AxiosLibrary/>}/>
+      <Route path='/cookie/cookie' element={<ReactCookieLibrary/>}/>
+      {/* 지원하지 않는 페이지에 대하여 처리 */}
+      <Route path='*' element={<h3>404 Error</h3>}/>
     </Routes>
   );
 }
